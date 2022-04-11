8e7afc6afcc89ff5e8f5b55fb7ab1778faf407fd
태정이 <85492054+taejung1@users.noreply.github.com>
2022-04-11 20:16:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+const nodemailer = require('nodemailer')
+const { id, pw, host } = require('./config.json')
 
+const from = "누구에서 메일이 오는지 , 예:(김태정 < taejung@taejung.xyz >"
+const to = "이메일을 보낼계정 아아디 , 예 : taejung@taejung.xyz"
+const subject = "메일제목"
+const text = "메일 내용"
+
+async function send() {
+
+    const account = nodemailer.createTransport({
+        host: host,
+        secure: true,
+        auth: {
+            user: id,
+            pass: pw
+        }
+    })
+
+    account.sendMail({ 
+        from: from,
+        to: to,
+        subject: subject,
+        text: text,  
+        //html:'<div>HTML형식으로 보낼 때 사용됩니다.</div>',  //html은 가렸습니다.
+       // attachments: files
+    })
+
+}
+
+send().catch(console.error)
